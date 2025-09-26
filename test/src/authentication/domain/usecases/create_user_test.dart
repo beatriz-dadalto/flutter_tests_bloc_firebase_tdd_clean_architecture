2e14d1bf119bbdf29fd9c50aaeabeb0b5b89256c
Beatriz Dadalto <beatrizdadalto@gmail.com>
2025-09-26 18:10:52 -0300

    understanding testing

---
+++ test/src/authentication/domain/usecases/create_user_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // 1. What does the class depend on
+  // 2. How can we create a fake version of the dependency
+  // 3. How do we control what our dependencies do
+  testWidgets('create user ...', (tester) async {
+    // TODO: Implement test
+  });
+}
